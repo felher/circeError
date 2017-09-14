e0bca9ea380584f15a50d2b1a2a1bc59ae6d8d76
Felix Herrmann <herrmann@olyro.de>
2017-09-14 15:41:39 +0200

    remove useless files

--- src/main/scala/org/felher/circeError/Export.scala
+++ src/main/scala/org/felher/circeError/Export.scala
@@ -1,24 +1,15 @@
 package org.felher.circeError
 
 final case class Export(
-  catalogEntries: Array[CatalogEntry],
   infos: Array[Info],
-  users: Array[User],
-  groups: Array[Group],
 )
 
 object Export {
   import io.circe._, generic.semiauto._
 
   // Those seem to be needed somehow. Not sure why.
-  //implicit val eA = CatalogEntry.enc
-  //implicit val aA = CatalogEntry.dec
   //implicit val eB = Info.enc
   //implicit val aB = Info.dec
-  //implicit val eC = User.enc
-  //implicit val aC = User.dec
-  //implicit val eD = Group.enc
-  //implicit val aD = Group.dec
 
   implicit val enc = deriveEncoder[Export]
   implicit val dec = deriveDecoder[Export]
